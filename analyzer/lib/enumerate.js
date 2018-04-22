c3cf987c8619e7a3f2949eac680475348687023f
William Shiao <willshiao@gmail.com>
2018-04-21 21:12:12 -0700

    Fix enumerate.js

--- analyzer/lib/enumerate.js
+++ analyzer/lib/enumerate.js
@@ -1,52 +1,50 @@
-var stoplist = require('stoplist.json');
-var Messages = require('../../parser/models/Message.js')
+const stoplist = require('stoplist.json')
+const Messages = require('../../parser/models/Message.js')
 
-//query is a json object ex {owner: 1}
-module.exports.enumerate = function(query){
-      var o = {},
-            self = this;
-      o.map = function () {
-            var dict = stoplist;
-            var newmsg = this.msg.toLowerCase().split(/[\[\]('")]+/).join('');
-            newmsg = newmsg.split(/[,\n.\s!?"'\t{}:;<>\/\\#$@%^&*()`~\-+=]+/);
+// query is a json object ex {owner: 1}
+module.exports.enumerate = function (query, cb) {
+  const o = {},
+    self = this
+  o.map = function () {
+    var dict = stoplist
+    var newmsg = this.msg.toLowerCase().split(/[\[\]('")]+/).join('')
+    newmsg = newmsg.split(/[,\n.\s!?"'\t{}:;<>\/\\#$@%^&*()`~\-+=]+/)
 
-            for (var i = 0; i < newmsg.length; i++) {
-                  if (newmsg[i].length < 1000 && newmsg[i].length > 0 && !dict[newmsg[i]])
-                        emit(newmsg[i], 1);
-                  }
-            }
-      };
-      o.reduce = function (k, vals) {
-            var total = 0;
+    for (var i = 0; i < newmsg.length; i++) {
+      if (newmsg[i].length < 1000 && newmsg[i].length > 0 && !dict[newmsg[i]]) {
+        emit(newmsg[i], 1)
+      }
+    }
+  }
 
-            for (var i = 0; i < values.length; i++) {
-                total += values[i];
-            }
-            return total;
-      };
-      o.query = query;
+  o.reduce = function (k, vals) {
+    var total = 0
 
-      Messages.mapReduce(o, function (err, results) {
-            if(err) throw err;
-            console.log(results)
-      });
-};
+    for (var i = 0; i < values.length; i++) {
+      total += values[i]
+    }
+    return total
+  }
+  o.query = query
+
+  return Messages.mapReduce(o, cb)
+}
 
 /*
 //out is a string indicating where the result should be output
 function enumerate(query, out) {
-      var extraobj = {};
+  var extraobj = {};
 
-      extraobj['query'] = query;
-      extraobj['out'] = out;
+  extraobj['query'] = query;
+  extraobj['out'] = out;
 
-      db.messages.mapReduce(
-            function() {
-                  
-            },
-            function(key,values) {
-                  
-            },
-            extraobj
-      )
-}*/
+  db.messages.mapReduce(
+    function() {
+
+    },
+    function(key,values) {
+
+    },
+    extraobj
+  )
+} */
