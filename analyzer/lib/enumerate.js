35460e83a18b7dca87e26d48bbc6f858041f9b7b
jjkjiang <jerryjiekaijiang@gmail.com>
2018-04-21 21:01:56 -0700

    mongoose

--- analyzer/lib/enumerate.js
+++ analyzer/lib/enumerate.js
@@ -1,6 +1,38 @@
 var stoplist = require('stoplist.json');
+var Messages = require('../../parser/models/Message.js')
 
 //query is a json object ex {owner: 1}
+module.exports.enumerate = function(query){
+      var o = {},
+            self = this;
+      o.map = function () {
+            var dict = stoplist;
+            var newmsg = this.msg.toLowerCase().split(/[\[\]('")]+/).join('');
+            newmsg = newmsg.split(/[,\n.\s!?"'\t{}:;<>\/\\#$@%^&*()`~\-+=]+/);
+
+            for (var i = 0; i < newmsg.length; i++) {
+                  if (newmsg[i].length < 1000 && newmsg[i].length > 0 && !dict[newmsg[i]])
+                        emit(newmsg[i], 1);
+                  }
+            }
+      };
+      o.reduce = function (k, vals) {
+            var total = 0;
+
+            for (var i = 0; i < values.length; i++) {
+                total += values[i];
+            }
+            return total;
+      };
+      o.query = query;
+
+      Messages.mapReduce(o, function (err, results) {
+            if(err) throw err;
+            console.log(results)
+      });
+};
+
+/*
 //out is a string indicating where the result should be output
 function enumerate(query, out) {
       var extraobj = {};
@@ -10,23 +42,11 @@ function enumerate(query, out) {
 
       db.messages.mapReduce(
             function() {
-                  var dict = stoplist;
-                  var newmsg = this.msg.toLowerCase().split(/[\[\]('")]+/).join('');
-                  newmsg = newmsg.split(/[,\n.\s!?"'\t{}:;<>\/\\#$@%^&*()`~\-+=]+/);
-
-                  for (var i = 0; i < newmsg.length; i++) {
-                        if (newmsg[i].length < 1000 && newmsg[i].length > 0 && !dict[newmsg[i]])
-                              emit(newmsg[i], 1);
-                  }
+                  
             },
             function(key,values) {
-                  var total = 0;
-
-                  for (var i = 0; i < values.length; i++) {
-                      total += values[i];
-                  }
-                  return total;
+                  
             },
             extraobj
       )
-}
+}*/
