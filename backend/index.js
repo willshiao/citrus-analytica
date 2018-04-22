c6d57cddea1abcdd8a75209b13f34f0e80ad0ae9
William Shiao <willshiao@gmail.com>
2018-04-21 20:58:20 -0700

    Start backend

---
+++ backend/index.js
@@ -0,0 +1,18 @@
+'use strict'
+
+const config = require('config')
+const express = require('express')
+const { ErrorHandler } = require('./lib/errorHandlers')
+
+require('./lib/extendExpress').extendResponse(express.response)
+const indexRoute = require('./routes/index')
+
+const app = express()
+app.use(indexRoute)
+// app.use(express.static('public'))
+app.use(ErrorHandler)
+
+const port = process.env.PORT || config.get('site.port')
+app.listen(port, () => {
+  console.log(`Listening on port #${port}`)
+})
